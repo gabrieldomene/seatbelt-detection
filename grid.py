a3809ee63daec8628488708e317b3bfb7b708131
Gabriel Domene <domenee.g@gmail.com>
2020-04-18 17:16:31 -0300

    Split images and flip copy

--- grid.py
+++ grid.py
@@ -8,34 +8,36 @@ path_images = os.path.join(os.getcwd(), "dataset")
 path_content = os.listdir(path_images)
 path_content = sample(path_content, len(path_content))
 
+count = 0
 while True:
-    
-    for idx, image in enumerate(path_content[:1]):
+    for idx, image in enumerate(path_content):
         '''Iterate over list of images'''
         name = os.path.join(path_images, image)
-        print("input: {}" .format(name))
         img = cv2.imread(name, 1)
 
         height, width = img.shape[:2]
-        # print("W:{} H:{}" .format(width, height))
         step_height = height//10
         step_width = width//10
-    
-        for step in range(1, 10):
-            print(step_height*step, step_width*step)
-            cv2.line(img, (0, step_height*step), (width, step_height*step), (0, 255, 0), 3)
-            cv2.line(img, (step_width*step, 0), (step_width*step, height), (255, 0, 0), 3)
+        
+        # Split in half and flip for dat aug
+        img_down = img[5*step_height:, :]
+        img_up = img[:5*step_height, :]
+        img_down_flip = cv2.flip(img_down, 1)
+        img_up_flip = cv2.flip(img_up, 1)
 
-        img1 = img[5*step_height:, :]
-        img2 = img[:5*step_height, :]
-        # img3 = img[5*step_height:height, :]
-        # img4 = img[5*step_height:height, :]
-
-        cv2.imwrite("down"+image, img1)
-        cv2.imwrite("up"+image, img2)
-
+        # cv2.imshow("Down region", cv2.resize(img_down, None, fx=0.3, fy=0.3))
+        # cv2.imshow("Down FLIPPED region", cv2.resize(img_down_flip, None, fx=0.3, fy=0.3))
+        # cv2.imshow("Up region", cv2.resize(img_up, None, fx=0.3, fy=0.3))
+        # cv2.imshow("Up FLIPPED region", cv2.resize(img_up_flip, None, fx=0.3, fy=0.3))
+        
+        path_save = os.path.join(os.getcwd(), "data_aug/")
+        cv2.imwrite(path_save+"{:04d}.png".format(count), img_down)
+        cv2.imwrite(path_save+"{:04d}.png".format(count+1), img_down_flip)
+        cv2.imwrite(path_save+"{:04d}.png".format(count+2), img_up)
+        cv2.imwrite(path_save+"{:04d}.png".format(count+3), img_up_flip)
+        count += 4
+        print("[Process] - Created: {:04d}/{:04d} - Iterating {:03d}/{}" .format(count, len(path_content)*4, idx+1, len(path_content)))
+     
     break
-    # if cv2.waitKey(1) & 0XFF == ord("q"):
-    #     break
 
 cv2.destroyAllWindows()
